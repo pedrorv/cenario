628cab451ba99554bc5f83a4a05b9b4887ee7fbb
Pedro Reis <pedroreisv@gmail.com>
2016-05-22 11:59:45 -0300

    First commit

---
+++ monthsAndMovies/js/specifications.js
@@ -0,0 +1,32 @@
+var visConfig = {
+  width: 1100,
+  height: 1200,
+  barSpacing: 12,
+  circleRadius: 5,
+  barMargin: 20,
+  wMargin: 100,
+  hMargin: 150,
+  months: {
+    1: "janeiro",
+    2: "fevereiro",
+    3: "março",
+    4: "abril",
+    5: "maio",
+    6: "junho",
+    7: "julho",
+    8: "agosto",
+    9: "setembro",
+    10: "outubro",
+    11: "novembro",
+    12: "dezembro"
+  },
+  years: {
+    "2009": 0,
+    "2010": 1,
+    "2011": 2,
+    "2012": 3,
+    "2013": 4,
+    "2014": 5,
+    "2015": 6
+  }
+};
